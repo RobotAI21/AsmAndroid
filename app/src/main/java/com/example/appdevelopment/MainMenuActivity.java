bb2db8e1029a4483514b74257c4e6f24995fd860
NgocSon <ngonson21725@gmail.com>
2025-07-18 09:18:09 +0700

    update

--- app/src/main/java/com/example/appdevelopment/MainMenuActivity.java
+++ app/src/main/java/com/example/appdevelopment/MainMenuActivity.java
@@ -4,11 +4,27 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainMenuActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ViewPager2 viewPager2;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main_menu);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        viewPager2 = findViewById(R.id.viewPager);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
     }
 }
