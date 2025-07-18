6dd5aa3a880e9001d683ba801b28c5755d5c2916
NgocSon <ngonson21725@gmail.com>
2025-07-18 10:10:33 +0700

    update

--- app/src/main/java/com/example/appdevelopment/MainMenuActivity.java
+++ app/src/main/java/com/example/appdevelopment/MainMenuActivity.java
@@ -1,8 +1,11 @@
 package com.example.appdevelopment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,7 +14,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     BottomNavigationView bottomNavigationView;
     ViewPager2 viewPager2;
     DrawerLayout drawerLayout;
@@ -26,5 +29,16 @@ public class MainMenuActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.nav_view);
+        //xu ly draw menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_Drawer, R.string.close_Drawe);
+                drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/appdevelopment/adapters/ViewPageAdapter.java
@@ -0,0 +1,40 @@
+package com.example.appdevelopment.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.appdevelopment.BudgetFragment;
+import com.example.appdevelopment.ExpensesFragment;
+import com.example.appdevelopment.HomeFragment;
+import com.example.appdevelopment.SettingFragment;
+
+public class ViewPageAdapter extends FragmentStateAdapter {
+
+    public ViewPageAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(position == 0){
+            return new HomeFragment();
+        } else if (position ==1 ) {
+            return new ExpensesFragment();
+        } else if (position==2) {
+            return new BudgetFragment();
+        } else if (position==3) {
+            return new SettingFragment();
+        } else {
+            return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
